24719b731d2144fd62b6a355cb42209f20500b76
shuhei <kawashu0723@gmail.com>
2016-04-17 12:32:27 +0900

    Initial Commit

---
+++ ReSwift-GitHub-Search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReSwift-GitHub-Search
+//
+//  Created by Shuhei Kawaguchi on 2016/04/17.
+//  Copyright © 2016年 Shuhei Kawaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
